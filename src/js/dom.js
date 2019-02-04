a9a23895f70700d5cd225bf2ec45476e401f146d
katherine <kretamales280@gmail.com>
2019-02-04 15:30:26 -0300

    Se agrego logeo con google

--- src/js/dom.js
+++ src/js/dom.js
@@ -26,6 +26,29 @@ window.onload = () => {
     loginUser(emailFromUser, passwordFromUser);
   })
 
+  document.getElementById('sign-google').addEventListener('click',
+  (evento)=>{
+    evento.preventDefault();
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  })
+
+
   document.getElementById('logout').addEventListener('click',
   (evento)=>{
     evento.preventDefault();
