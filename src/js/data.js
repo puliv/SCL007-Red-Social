273b3afc10925872a09fb1eca4261e3d8c27e1f2
katherine <kretamales280@gmail.com>
2019-02-05 13:25:14 -0300

    Se agrego guardar post y guardar datos prsonales

---
+++ src/js/data.js
@@ -0,0 +1,22 @@
+const registerPost = (postText, postStatus) => {
+    const newPostKey = firebase.database().ref('users/post/').child('post').push().key;
+    firebase.database().ref(`users/post/${newPostKey}`).set({
+      post : postText,
+      status : postStatus
+    });
+};
+
+const readPost = (onPostChange) => {
+    const postRef = firebase.database().ref('users/post');
+    postRef.on('child_added', (post)=> {
+        onPostChange(post);
+    });
+  };
+
+const  settingsPage =(email,username,sport)=>{
+    firebase.database().ref("users/" + firebase.auth().currentUser.uid).update({
+        email:email,
+        userName: username,
+        sport: sport
+    });
+}
