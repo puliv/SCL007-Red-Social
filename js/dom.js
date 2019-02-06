03df82ca3d20c509c647bbe16c4f84a499600da0
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-02-06 17:20:39 -0300

    subiendo logos

--- js/dom.js
+++ js/dom.js
@@ -4,13 +4,13 @@ window.onload = () => {
     if(user){
       start.style.display = "none";
       app.style.display = "block";
-      logout.style.display = "block";
+      logout_btn.style.display = "block";
       readPostFromDatabase();
     }else{
       start.style.display = "block";
       app.style.display = "none";
       settingProfile.style.display = "none";
-      logout.style.display = "none";
+      logout_btn.style.display = "none";
     }
   });
  
@@ -115,5 +115,5 @@ document.getElementById('settingProfile').addEventListener('click',
     start.style.display = "none";
     app.style.display = "none";
     settingProfile.style.display = "block";
-    logout.style.display = "none";
+    logout_btn.style.display = "none";
   })
