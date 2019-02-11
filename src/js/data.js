82233317f5b312f549f3fd93ac8d1c136e585512
katherine <kretamales280@gmail.com>
2019-02-11 16:16:35 -0300

    Se agrego comentario privado y publico

--- src/js/data.js
+++ src/js/data.js
@@ -1,22 +1,35 @@
-const registerPost = (postText, postStatus) => {
+const  settingsPage =(email,username,birthdate,sport)=>{
+    firebase.database().ref("users/" + firebase.auth().currentUser.uid).update({
+        email:email,
+        userName: username,
+        birthdate: birthdate,
+        sport: sport
+    })
+    .then(() => {
+        alert(username + " sus datos se registraron correctamente");
+    })
+    .catch((error)  => {
+        console.error("Error > " + error.message);
+    });
+}
+
+const registerPost = (postText, postStatus,email) => {
     const newPostKey = firebase.database().ref('users/post/').child('post').push().key;
-    firebase.database().ref(`users/post/${newPostKey}`).set({
+    firebase.database().ref(`users/${firebase.auth().currentUser.uid}/post/${newPostKey}`).set({
       post : postText,
-      status : postStatus
+      status : postStatus,
+      email: email
+    }).then(() => {
+        alert(firebase.auth().currentUser.email + " se ha publicado");
+    })
+    .catch((error)  => {
+        console.error("Error > " + error.message);
     });
 };
 
 const readPost = (onPostChange) => {
-    const postRef = firebase.database().ref('users/post');
+    const postRef = firebase.database().ref(`users/${firebase.auth().currentUser.uid}/post`);
     postRef.on('child_added', (post)=> {
         onPostChange(post);
     });
   };
-
-const  settingsPage =(email,username,sport)=>{
-    firebase.database().ref("users/" + firebase.auth().currentUser.uid).update({
-        email:email,
-        userName: username,
-        sport: sport
-    });
-}
