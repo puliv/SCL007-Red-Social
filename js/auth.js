866bcc6b6a8e3928c12089b70a194eeeab972e94
katherine <kretamales280@gmail.com>
2019-02-11 16:16:35 -0300

    Se agrego comentario privado y publico

--- js/auth.js
+++ js/auth.js
@@ -13,14 +13,14 @@ const checkAuthState = (callback) => {
 //funcion que registra usuario
 const registerUser = (email, password) => {
     firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((user) => {
-            alert("Usuario registrado");
+        .then(() => {
+            alert("Bienvenida a LAW");
         })
         .catch((error) => {
             alert("Error > " + error.message);
         });
  }
- 
+ //funcion iniciar sesion con firebase
  const loginUser =(email, password) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then((user) => {
@@ -28,5 +28,41 @@ const registerUser = (email, password) => {
     })
     .catch((error)  => {
         console.error("Error > " + error.message);
+    });
+ }
+ //funcion iniciar sesion con google
+ const loginUserGoogle =() => {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      let user = result.user;
+      console.log(user);
+    }).catch(function(error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      let email = error.email;
+      let credential = error.credential;
+      console.log(errorCode + errorMessage + email + credential)
+    });
+ }
+ //funcion iniciar sesion con facebook
+const loginUserFacebook=() => {
+    let provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      let user = result.user;
+      console.log(user);
+    }).catch(function(error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      let email = error.email;
+      let credential = error.credential;
+      console.log(errorCode + errorMessage + email + credential)
+    });
+ }
+//funcion cerrar sesion
+ const signOff =() =>{
+    firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    }).catch(function(error) {
+    // An error happened.
     });
  }
