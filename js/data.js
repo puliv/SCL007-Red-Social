b3758bafd73f43c2d0f22171558dc9d7fec90e9c
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-02-11 04:12:17 -0300

    Trabajando con Eliminar Post

--- js/data.js
+++ js/data.js
@@ -7,11 +7,40 @@ const registerPost = (postText, postStatus) => {
 };
 
 const readPost = (onPostChange) => {
-    const postRef = firebase.database().ref('users/post');
+    const postRef = firebase.database().ref('users/post/');
     postRef.on('child_added', (post)=> {
         onPostChange(post);
     });
   };
+
+//Borrar un post
+let deletePost = firebase.database().ref('users/post/');
+let alert = confirm('Seguro deseas eliminar tu comentario?'); 
+    if (alert == true) {
+        deletePost.remove();
+	} else {
+        return  null 
+    }
+
+
+//const deletePost = removePost.child('users/post');
+    // Delete the file
+  //  deletePost.delete().then(function(){
+    //}).catch(function(error){
+  //  Uh-oh, an error occurred!
+  //  });
+
+// Create a reference to the file to delete
+//var desertRef = storageRef.child('images/desert.jpg');
+
+// Delete the file
+//desertRef.delete().then(function() {
+  // File deleted successfully
+//}).catch(function(error) {
+  // Uh-oh, an error occurred!
+//});
+
+
 
 const  settingsPage =(email,username,sport)=>{
     firebase.database().ref("users/" + firebase.auth().currentUser.uid).update({
