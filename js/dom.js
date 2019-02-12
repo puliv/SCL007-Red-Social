b94d20fa7b5d50cbef4b86fda9b77c6a2d530674
puliv <pverapavez@gmail.com>
2019-02-12 18:29:38 -0300

    cambios recibidos

--- js/dom.js
+++ js/dom.js
@@ -62,22 +62,7 @@ window.onload = () => {
       event.preventDefault();
       signOff();
     })
-  //boton publicar 
-  // document.getElementById('state_btn').addEventListener('click',
-  //     (event) => {
-  //         event.preventDefault();
-  //         const contect = textareaContect.value;
-  //         const radios = document.getElementsByName('state');
-  //         const email = firebase.auth().currentUser.email;
-  //         for (let i = 0; i < radios.length; i++) {
-  //             if (radios[i].checked) {
-  //                let statusRadio = radios[i].value;
-  //                 break;
-  //             }
-  //         }
-  //         registerPost(contect, statusRadio, email);
-  //     });
-
+  //boton publicar
   document.getElementById('state_button').addEventListener('click',
     (event) => {
       event.preventDefault();
@@ -92,34 +77,23 @@ window.onload = () => {
       }
       registerPost(contect, statusRadio, email);
     })
-
   const readPostFromDatabase = () => {
     postContainer.innerHTML = "";
     readPost((post) => {
       postContainer.innerHTML +=
-        `<h6>Publicación de:${post.val().email}
+        `<h6>Publicación de:${post.val().email}</h6>
                <input type="text" value="${post.val().post}">
                <h6>${post.val().status}</h6>
                <h6>${post.key}</h6>
-
-                 <button type="button" id="${post.key}">Comentar</button>
-                 <button type="button" id="delete_btn${post.key}" class="deletePost">Eliminar</button>`; //eliminar post
+               <button type="button" id="${post.key}">Comentar</button>
+               <button type="button" id="delete_btn${post.key}" class="deletePost">Eliminar</button>
+              <button id="edit_${post.key}" type="button" >Editar</button>`;//eliminar post
       //hago una coleccion de botones
       let coleccButton = document.getElementsByClassName("deletePost");
       for (let i = 0; i < coleccButton.length; i++) {
         coleccButton[i].addEventListener("click", deletePost);
       }
-      //   const p="delete_btn"+post.key;
-      //     document.getElementById(p).addEventListener('click',(event)=>{
-      //         event.preventDefault();
-      //         let nuevop = p.substring(10,50);
-      //         console.log(p);
 
-      //        // console.log(post.key);
-      //         // let nuevop=p.substring(11,20);
-      //         // confirm(nuevop);
-      //         deletePost(nuevop);
-      //     });
     });
 
   }
